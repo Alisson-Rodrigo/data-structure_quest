55cf24024332531af3eaf245ffa7f0a180ad2b07
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2024-01-05 12:25:58 -0300

    ll

--- animal.h
+++ animal.h
@@ -2,10 +2,10 @@ typedef struct animal Animal;
 
 Animal *criaListaEncadeadaSimplesAnimais();
 Animal *cadastrar(Animal *rebanho);
-//Animal *permutasAnimais(Fazenda *origem, Fazenda *destino, int id_animal);//obervar para atualizar o status do animal na fazenda de destino
-//Animal *remover(Fazenda *fazenda, int id_animal);
+Animal *permutasAnimais(Fazenda *origem, Fazenda *destino, int id_animal);//obervar para atualizar o status do animal na fazenda de destino
+Animal *remover(Fazenda *fazenda, int id_animal);
 /*
 Desenvolva funcoes que:
 
-- Funcao para liberar a memoria
+- Funcao para liberar a memoria // ok
 */
