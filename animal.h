9e290983347c9bfe1acc6293ab80f7fc3c0f35ad
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-01-05 15:10:37 -0300

    ki

--- animal.h
+++ animal.h
@@ -5,6 +5,8 @@ Animal *cadastrar(Animal *rebanho);
 //Animal *permutasAnimais(Fazenda *origem, Fazenda *destino, int id_animal);//obervar para atualizar o status do animal na fazenda de destino
 //Animal *remover(Fazenda *fazenda, int id_animal);
 void imprimir(Animal *rebanho);
+void liberar(Animal *rebanho);
+
 /*
 Desenvolva funcoes que:
 
